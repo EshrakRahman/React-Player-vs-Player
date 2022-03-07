0a2b1b11a5b3ebdfe1e7d1b207266d8ac9f284ea
eshrakrahman <eshrakurrahman@gmail.com>
2022-03-07 20:47:09 +0600

    html structure builded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className={'main'}>
+            <h1 className={'title'}>Player vs Player Game</h1>
+            <p>Winning Score: 7</p>
+            <div className={'players-score'}>
+                <h3>Player One: 1</h3>
+                <h3>Player Two: 2</h3>
+            </div>
+            <input type='text' className={'input'}/>
+        </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import App from "./App";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Index = () => {
+    return (
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+export default Index;
